efd015d876341e8e9b0900d34c46892c83643e5c
Vladyslav Yeroma <101885169+vVladyslaVv@users.noreply.github.com>
2023-06-05 17:38:50 +0200

    Added Comments

--- Stack/22_Generate_Parentheses.py
+++ Stack/22_Generate_Parentheses.py
@@ -1,6 +1,8 @@
 from typing import List
 
-
+# Let's use backtracking here. Inside backtracking function we create local variables for opened and closed
+# parentheses We basically append to result when opened = closed = n, if not we recursively call backtrack if opened
+# < n or closed < opened with parameters like opened + 1 or closed + 1, and then pop to execute the backtracking itself
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         stack = []
