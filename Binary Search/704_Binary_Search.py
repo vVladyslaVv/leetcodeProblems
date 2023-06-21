bd4cc68735bc7c784ad48932500d66aeec26427f
Vladyslav Yeroma <101885169+vVladyslaVv@users.noreply.github.com>
2023-06-21 15:58:00 -0400

    Added Comments

--- Binary Search/704_Binary_Search.py
+++ Binary Search/704_Binary_Search.py
@@ -1,6 +1,9 @@
 from typing import List
 
-
+# We use 2 pointers and divide and conquer algorithms
+# As our array is sorted, each time we calculate the midway point
+# and compare it to the target, that way we no which pointer we have to shift
+# We do it until our midway meats the target
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         left, right = 0, len(nums) - 1
