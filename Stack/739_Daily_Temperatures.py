cd48dfb330ec4b97b364044d7064afd7aa83816c
Vladyslav Yeroma <101885169+vVladyslaVv@users.noreply.github.com>
2023-06-21 15:37:02 -0400

    Added Comments

--- Stack/739_Daily_Temperatures.py
+++ Stack/739_Daily_Temperatures.py
@@ -1,6 +1,11 @@
 from typing import List
 
-
+# First of all we create an empty stack an empty result list.
+# Then we iterate threw all temperatures and if stack is not empty and current temperature is warmer
+# then the temperature under the index, last pushed to the stack, then we can pop the stack and on the
+# popped index in the result list we will place current index - popped index (number of days to get warmer)
+# While ensures that if temperature is bigger then all temperatures before, it will pop the stack until there
+# is no smaller temperature behind
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         stack = []
