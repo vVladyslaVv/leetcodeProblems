aa4d523238e05fa94cb1924cec8bff48cd1df57b
Vladyslav Yeroma <101885169+vVladyslaVv@users.noreply.github.com>
2023-06-26 08:45:08 -0400

    Added Longest Repeating Character Replacement

---
+++ Sliding Window/424_Longest_Repeating_Character_Replacement.py
@@ -0,0 +1,37 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+
+        left = 0
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+
+            while (right - left  + 1) - max(count.values()) > k:
+                count[s[left]] -= 1
+                left += 1
+
+            res = max(res, right - left  + 1)
+
+        return res
+
+
+# Fast optimization
+class Solution2:
+    def characterReplacement(self, s: str, k: int) -> int:
+        count = {}
+        res = 0
+
+        left = 0
+        maxF = 0
+        for right in range(len(s)):
+            count[s[right]] = 1 + count.get(s[right], 0)
+            maxF = max(maxF, count[s[right]])
+
+            while (right - left  + 1) - maxF > k:
+                count[s[left]] -= 1
+                left += 1
+
+            res = max(res, right - left  + 1)
+
+        return res
